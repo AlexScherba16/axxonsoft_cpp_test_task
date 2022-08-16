d2345dc03a551f3af66153d02d3528031a56db07
Alex <alexscherba16@gmail.com>
2022-08-16 19:08:59 +0300

    Implemented application launcher

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,13 @@
+#include "application/default_application/default_application.h"
 #include <iostream>
+#include <memory>
 
 int main(int argc, char *argv[]){
+    using namespace application::default_application;
+
     try {
-        (void) argc;
-        (void) argv;
-
-
-        return EXIT_SUCCESS;
+        std::unique_ptr<application::IRunnable> app(new DefaultApplication());
+        return app->run(argc, argv);
     } catch(const std::exception& ex) {
         std::cout << ex.what() << std::endl;
         return EXIT_FAILURE;
